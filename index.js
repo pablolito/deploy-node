7eb7f912a4738b7bb3b4d283e7240f48a3e2dd43
mfalguierwcs <maxime.falguier@wildcodeschool.com>
2023-04-20 15:21:37 +0200

    test deploy

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({
-    msg: "Hello !"
-  })
+  res.send(<h1>Hello ;)</h1>)
 })
 
 const PORT = 3000
